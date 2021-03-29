6f3c313df3b6d86910c8d8f8ee3d01657df82a6c
Raphael Dias <raphaeldiasc16@gmail.com>
2021-03-29 00:08:36 -0300

    tomadas

--- assets/validacoes.js
+++ assets/validacoes.js
@@ -1,4 +1,5 @@
 function validateMoviment(coordDestino, coordOrigem, corPeca){
+    
     var x_destino = parseInt(coordDestino["row"]);
     var y_destino = parseInt(coordDestino["column"]);
 
@@ -10,26 +11,300 @@ function validateMoviment(coordDestino, coordOrigem, corPeca){
         if(corPeca == 1){//se for azul deve andar o valor da coordenada atual(x) + 1 && coordenada atual(y) - 1 ou coordenada atual(x) + 1 && coordenada atual(y) + 1
             if(x_destino == x_origem+1 && y_destino == y_origem-1 || x_destino == x_origem+1 && y_destino == y_origem+1){
                 return true
+            }else if((x_destino - 2) == x_origem){
+                if(y_destino > y_origem){
+                    console.log(document.getElementById((x_destino - 2)+ ""+ (y_destino-2)).childNodes.length)
+                    if(document.getElementById(x_destino + ""+ y_destino).childNodes.length != 1){
+                        let casa = (x_destino - 1)+ ""+ (y_destino-1)
+                        if(document.getElementById(casa).firstChild){
+                            console.log(document.getElementById(casa).firstChild.id)
+                        
+                            document.getElementById(casa).removeChild(document.getElementById(casa).firstChild)
+                            return true
+                        }else{
+                            console.log("nao é valido")
+                            return false
+                        }
+                    }else{
+                        return false
+                    }
+                }else{
+                    console.log(document.getElementById((x_destino + 2)+ ""+ (y_destino+2)).childNodes.length)
+                    if(document.getElementById(x_destino + ""+ y_destino).childNodes.length != 1){
+                    let casa = (x_destino - 1)+ ""+ (y_destino+1)
+                    if(document.getElementById(casa).firstChild){
+                        console.log(document.getElementById(casa).firstChild.id)
+                            document.getElementById(casa).removeChild(document.getElementById(casa).firstChild)
+                            return true
+                        }else{
+                            console.log("nao é valido")
+                            return false
+                        }
+                    }else{
+                        return false
+                    }
+                }
+                //console.log((x_destino + 1) + "," + (y_destino -1))
+            }else if(x_destino == x_origem-2 && y_destino == y_origem-2 || x_destino == x_origem-2 && y_destino == y_origem+2){
+                if(y_destino > y_origem){
+                    console.log(document.getElementById((x_destino - 2)+ ""+ (y_destino-2)).childNodes.length)
+                    if(document.getElementById(x_destino + ""+ y_destino).childNodes.length != 1){
+                        let casa = (x_destino + 1)+ ""+ (y_destino-1)
+                        if(document.getElementById(casa).firstChild){
+                            console.log(document.getElementById(casa).firstChild.id)
+                        
+                            document.getElementById(casa).removeChild(document.getElementById(casa).firstChild)
+                            return true
+                        }else{
+                            console.log("nao é valido")
+                            return false
+                        }
+                    }else{
+                        return false
+                    }
+                }else{
+                    console.log(document.getElementById((x_destino + 2)+ ""+ (y_destino+2)).childNodes.length)
+                    if(document.getElementById(x_destino + ""+ y_destino).childNodes.length != 1){
+                    let casa = (x_destino + 1)+ ""+ (y_destino+1)
+                    if(document.getElementById(casa).firstChild){
+                        console.log(document.getElementById(casa).firstChild.id)
+                            document.getElementById(casa).removeChild(document.getElementById(casa).firstChild)
+                            return true
+                        }else{
+                            console.log("nao é valido")
+                            return false
+                        }
+                    }else{
+                        return false
+                    }
+                }
             }
+
         }else if(corPeca == 2){
             if(x_destino == x_origem-1 && y_destino == y_origem-1 || x_destino == x_origem-1 && y_destino == y_origem+1){
                 return true
+            }else if((x_destino + 2) == x_origem){
+                if(y_destino > y_origem){
+                    console.log(document.getElementById((x_destino + 2)+ ""+ (y_destino-2)).childNodes.length)
+                    if(document.getElementById(x_destino + ""+ y_destino).childNodes.length != 1){
+                        let casa = (x_destino + 1)+ ""+ (y_destino-1)
+                        if(document.getElementById(casa).firstChild){
+                            console.log(document.getElementById(casa).firstChild.id)
+                            document.getElementById(casa).removeChild(document.getElementById(casa).firstChild)
+                            return true
+                        }else{
+                            console.log("nao é valido")
+                            return false
+                        }
+                    }else{
+                        return false
+                    }
+                }else{
+                    console.log((x_destino + 2)+ ""+ (y_destino+2))
+                   console.log(document.getElementById((x_destino + 2)+ ""+ (y_destino+2)).childNodes.length)
+                   if(document.getElementById(x_destino + ""+ y_destino).childNodes.length != 1){
+                    let casa = (x_destino + 1)+ ""+ (y_destino+1)
+                    if(document.getElementById(casa).firstChild){
+                        console.log(document.getElementById(casa).firstChild.id)
+                            document.getElementById(casa).removeChild(document.getElementById(casa).firstChild)
+                            return true
+                        }else{
+                            console.log("nao é valido")
+                            return false
+                        }
+                    }else{
+                        return false
+                    }
+                }
+                //console.log((x_destino + 1) + "," + (y_destino -1))
+            }else if(x_destino == x_origem+2 && y_destino == y_origem-2 || x_destino == x_origem+2 && y_destino == y_origem+2){
+                if(y_destino > y_origem){
+                    console.log(document.getElementById((x_destino - 2)+ ""+ (y_destino-2)).childNodes.length)
+                    if(document.getElementById(x_destino + ""+ y_destino).childNodes.length != 1){
+                        let casa = (x_destino - 1)+ ""+ (y_destino-1)
+                        if(document.getElementById(casa).firstChild){
+                            console.log(document.getElementById(casa).firstChild.id)
+                        
+                            document.getElementById(casa).removeChild(document.getElementById(casa).firstChild)
+                            return true
+                        }else{
+                            console.log("nao é valido")
+                            return false
+                        }
+                    }else{
+                        return false
+                    }
+                }else{
+                    console.log(document.getElementById((x_destino + 2)+ ""+ (y_destino+2)).childNodes.length)
+                    if(document.getElementById(x_destino + ""+ y_destino).childNodes.length != 1){
+                    let casa = (x_destino - 1)+ ""+ (y_destino+1)
+                    if(document.getElementById(casa).firstChild){
+                        console.log(document.getElementById(casa).firstChild.id)
+                            document.getElementById(casa).removeChild(document.getElementById(casa).firstChild)
+                            return true
+                        }else{
+                            console.log("nao é valido")
+                            return false
+                        }
+                    }else{
+                        return false
+                    }
+                }
             }
         }
         
     }
 
     if(x_destino % 2 != 0 && y_destino % 2 == 0){
+        console.log("AQUI 2")
+
+        console.log("x_destino debaixo: " + x_destino)
         //Falta antes do IF abaixo a validação se é dama ou não, pois caso seja as regras abaixo nao se aplicarão
         if(corPeca == 1){//se for azul deve andar o valor da coordenada atual(x) + 1 && coordenada atual(y) - 1 ou coordenada atual(x) + 1 && coordenada atual(y) + 1
             if(x_destino == x_origem+1 && y_destino == y_origem-1 || x_destino == x_origem+1 && y_destino == y_origem+1){
                 return true
+            }else if((x_destino - 2) == x_origem){
+
+                if(y_destino > y_origem){
+                    if(document.getElementById(x_destino + ""+ y_destino).childNodes.length != 1){
+                        let casa = (x_destino - 1)+ ""+ (y_destino-1)
+                        if(document.getElementById(casa).firstChild){
+                            console.log(document.getElementById(casa).firstChild.id)
+                            document.getElementById(casa).removeChild(document.getElementById(casa).firstChild)
+                            return true
+                        }else{
+                            console.log("nao é valido")
+                            return false
+                        }
+                    }else{
+                        return false
+                    }
+                }else{
+                    if(document.getElementById(x_destino + ""+ y_destino).childNodes.length != 1){
+                    let casa = (x_destino - 1)+ ""+ (y_destino+1)
+                    if(document.getElementById(casa).firstChild){
+                        console.log(document.getElementById(casa).firstChild.id)
+                            document.getElementById(casa).removeChild(document.getElementById(casa).firstChild)
+                            return true
+                        }else{
+                            console.log("nao é valido")
+                            return false
+                        }
+                    }else{
+                        return false
+                    }
+                }
+                //console.log((x_destino + 1) + "," + (y_destino -1))
+            }else if(x_destino == x_origem-2 && y_destino == y_origem-2 || x_destino == x_origem-2 && y_destino == y_origem+2){
+                if(y_destino > y_origem){
+                    if(document.getElementById(x_destino + ""+ y_destino).childNodes.length != 1){
+                        let casa = (x_destino + 1)+ ""+ (y_destino-1)
+                        if(document.getElementById(casa).firstChild){
+                            console.log(document.getElementById(casa).firstChild.id)
+                        
+                            document.getElementById(casa).removeChild(document.getElementById(casa).firstChild)
+                            return true
+                        }else{
+                            console.log("nao é valido")
+                            return false
+                        }
+                    }else{
+                        return false
+                    }
+                }else{
+                    if(document.getElementById(x_destino + ""+ y_destino).childNodes.length != 1){
+                    let casa = (x_destino + 1)+ ""+ (y_destino+1)
+                    if(document.getElementById(casa).firstChild){
+                        console.log(document.getElementById(casa).firstChild.id)
+                            document.getElementById(casa).removeChild(document.getElementById(casa).firstChild)
+                            return true
+                        }else{
+                            console.log("nao é valido")
+                            return false
+                        }
+                    }else{
+                        return false
+                    }
+                }
             }
+
         }else if(corPeca == 2){
             if(x_destino == x_origem-1 && y_destino == y_origem-1 || x_destino == x_origem-1 && y_destino == y_origem+1){
                 return true
+            }else if((x_destino + 2) == x_origem){
+
+                if(y_destino > y_origem){
+                         console.log(document.getElementById((x_destino + 2)+ ""+ (y_destino-2)).childNodes.length)
+                        if(document.getElementById(x_destino + ""+ y_destino).childNodes.length != 1){
+                        let casa = (x_destino + 1)+ ""+ (y_destino-1)
+                        if(document.getElementById(casa).firstChild){
+                            console.log(document.getElementById(casa).firstChild.id)
+                            document.getElementById(casa).removeChild(document.getElementById(casa).firstChild)
+                            return true
+                        }else{
+                            console.log("nao é valido")
+                            return false
+                        }
+                    }else{
+                        return false
+                    }
+                }else{
+                    console.log(document.getElementById((x_destino + 2)+ ""+ (y_destino+2)).childNodes.length)
+                    if(document.getElementById(x_destino + ""+ y_destino).childNodes.length != 1){
+                    let casa = (x_destino + 1)+ ""+ (y_destino+1)
+                    if(document.getElementById(casa).firstChild){
+                        console.log(document.getElementById(casa).firstChild.id)
+                            document.getElementById(casa).removeChild(document.getElementById(casa).firstChild)
+                            return true
+                        }else{
+                            console.log("nao é valido")
+                            return false
+                        }
+                    }else{
+                        return false
+                    }
+                }
+                //console.log((x_destino + 1) + "," + (y_destino -1))
+            }else if(x_destino == x_origem+2 && y_destino == y_origem-2 || x_destino == x_origem+2 && y_destino == y_origem+2){
+                if(y_destino > y_origem){
+                    console.log(document.getElementById((x_destino - 2)+ ""+ (y_destino-2)).childNodes.length)
+                    if(document.getElementById(x_destino + ""+ y_destino).childNodes.length != 1){
+                        let casa = (x_destino - 1)+ ""+ (y_destino-1)
+                        console.log("ORIGENS: " + "("+ x_origem + ","+y_origem + ")")
+                        console.log(casa);
+                        if(document.getElementById(casa).firstChild){
+                            
+                            console.log(document.getElementById(casa).firstChild.id)
+                        
+                            document.getElementById(casa).removeChild(document.getElementById(casa).firstChild)
+                            return true
+                        }else{
+                            console.log("nao é valido")
+                            return false
+                        }
+                    }else{
+                        return false
+                    }
+                }else{
+                    console.log(document.getElementById((x_destino + 2)+ ""+ (y_destino+2)).childNodes.length)
+                    if(document.getElementById(x_destino + ""+ y_destino).childNodes.length != 1){
+                    let casa = (x_destino - 1)+ ""+ (y_destino+1)
+                    if(document.getElementById(casa).firstChild){
+                        console.log(document.getElementById(casa).firstChild.id)
+                            document.getElementById(casa).removeChild(document.getElementById(casa).firstChild)
+                            return true
+                        }else{
+                            console.log("nao é valido")
+                            return false
+                        }
+                    }else{
+                        return false
+                    }
+                }
             }
         }
+
+        
     }
 
     return false
